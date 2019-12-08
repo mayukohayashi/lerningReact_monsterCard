9c0de48a93cd7ab44322024968eaa51650d63246
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-08 16:58:11 +0900

    Updates

---
+++ precache-manifest.a9ce563aa9f2220f119ad96b54c33c11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a14ef3eff9b07bda6e126ad50ec80e5",
+    "url": "/lerningReact_monsterCard/index.html"
+  },
+  {
+    "revision": "5bc3e67f6377333c4697",
+    "url": "/lerningReact_monsterCard/static/css/main.f2fc164d.chunk.css"
+  },
+  {
+    "revision": "f773e489f6fefb0efc0c",
+    "url": "/lerningReact_monsterCard/static/js/2.36aaefe9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/lerningReact_monsterCard/static/js/2.36aaefe9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5bc3e67f6377333c4697",
+    "url": "/lerningReact_monsterCard/static/js/main.44a0cb1c.chunk.js"
+  },
+  {
+    "revision": "cfc8cd3ac88dfe468683",
+    "url": "/lerningReact_monsterCard/static/js/runtime-main.3bd14e61.js"
+  }
+]);
