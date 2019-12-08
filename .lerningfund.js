01f943aaba142addac683cdc84542c0cd1b9af26
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-08 16:45:55 +0900

    learning react

---
+++ .lerningfund.js
@@ -0,0 +1,145 @@
+import { getQueriesForElement } from "@testing-library/dom"
+import { getHeapCodeStatistics } from "v8"
+import { resolve } from "dns"
+
+/* eslint-disable */
+
+// map
+const myArray = [1,2,3,4,5]
+undefined
+
+myArray.map(el => el * 3)
+(5) [3, 6, 9, 12, 15]
+
+myArray.map(() => 'b')
+(5) ["b", "b", "b", "b", "b"]
+
+
+// promises
+getQueriesForElement('twitter/mayuko', (user, error) => {
+  if (error) {
+    throw(error)
+  }
+
+  const userId = user.id
+
+  getFriend(userId, (friends, error) => {
+    if (error) {
+      throw(error)
+    };
+
+    const john = friends.find()
+
+    getPosts(john, (posts, error) =>{
+      if (error) {
+        throw(error)
+      }
+
+      const recentPost = posts[0];
+
+      getCommentsForPost(recentPost, (comments, error) => {
+        if (error) {
+          throw(error)
+        }
+
+        displayComments(comments);
+      })
+    })
+  })
+})
+
+const myPromise = new Promise((resolve, reject) => {
+  if (false) {
+    setTimeout(() => {
+      resolve('I got it');
+    }, 1000);
+  } else {
+    reject('I have failed')
+  }
+})
+
+myPromise
+  .then(value => console.log(value)
+  .catch(rejectValue => console.log(rejectValue)))
+
+
+// filter()
+const myArray = [1,3,5,7,9,10]
+undefined
+
+myArray.filter(el => el > 5)
+(3) [7, 9, 10]
+
+myArray.filter(el => el = true)
+(6) [1, 3, 5, 7, 9, 10]
+
+myArray
+(6) [1, 3, 5, 7, 9, 10]
+
+
+// scope JS THIS!
+const newFunc = () => console.log(this)
+undefined
+
+newFunc()
+VM2238:1 Window {parent: Window, postMessage: ƒ, blur: ƒ, focus: ƒ, close: ƒ, …}
+undefined
+
+
+//includes()
+const myArray = [1,2,3,4,5,6,7]
+undefined
+myArray.includes(3)
+true
+myArray.includes(9)
+false
+const newArray = [{id: 1}, {id: 2}, {id: 3}]
+undefined
+newArray(1)
+VM2495:1 Uncaught TypeError: newArray is not a function
+    at <anonymous>:1:1
+(anonymous) @ VM2495:1
+newArray[1]
+{id: 2}
+myArray.includes(2, 4)
+false
+newArray.includes({id: 2})
+false
+var b = 3
+undefined
+var c = b
+undefined
+c
+3
+var b = 5
+undefined
+c
+3
+const obj1 = {id:1}
+undefined
+const obj2 = {id:1}
+undefined
+obj1 === obj2
+false
+const obj3 = obj2
+undefined
+obj2 === obj3
+true
+obj2.id = 17
+17
+obj2
+{id: 17}
+obj3
+{id: 17}
+const o1 = {id: 1}
+undefined
+const o2 = {id: 2}
+undefined
+const o3 = {id: 3}
+undefined
+const myNewArray = [o1, o2, o3]
+undefined
+myNewArray
+(3) [{…}, {…}, {…}]0: {id: 1}1: {id: 2}2: {id: 3}length: 3__proto__: Array(0)
+myNewArray.includes(o1)
+true
---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+
+import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
+
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      monsters:[],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response =>response.json())
+    .then(users => this.setState({monsters: users}))
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    // const monsters = this.state.monsters
+    // const searchField = this.state.searchField
+
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+    return (
+
+    // JSX
+    // inside{} javascript expression
+    <div className="App">
+      <h1> Monster Rolodex </h1>
+      <SearchBox
+        placeholder='search monsters'
+        handleChange={this.handleChange}
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+
+    )
+  }
+}
+
+export default App;
